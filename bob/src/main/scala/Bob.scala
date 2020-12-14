3091a73333dae3a31df8f8463949f9db0b8d2d07
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2020-12-14 15:13:14 +0100

    pattern matching

--- bob/src/main/scala/Bob.scala
+++ bob/src/main/scala/Bob.scala
@@ -1,15 +1,15 @@
-import scala.util.matching.Regex
-
 object Bob {
-  val patternAnswerMap: Map[Regex, String] = Map(
-    "^[^a-z]+$".r -> "Whoa, chill out!", // any non-lowercase characters
-    "^.*\\?$".r -> "Sure.", // any sequence of characters ended by a question mark
-    "^[ ]*$".r -> "Fine. Be that way!", // Empty string: 0 to multiple blanks
-  )
-  val defaultAnswer: String = "Whatever."
-  def response(statement: String): String = {
-    val matchedPattern = patternAnswerMap.keys.filter(_.findFirstIn(statement).isDefined)
-    if (matchedPattern.isEmpty) defaultAnswer
-    else patternAnswerMap(matchedPattern.toSeq.head)
+  def response(statement: String): String = statement.trim match {
+    case s if s.isEmpty => "Fine. Be that way!"
+    case s if shouting(s) && questioning(s) => "Calm down, I know what I'm doing!"
+    case s if shouting(s) => "Whoa, chill out!"
+    case s if questioning(s) => "Sure."
+    case _ => "Whatever."
   }
+
+  def shouting(statement: String): Boolean = statement.toUpperCase == statement && hasLetters(statement)
+
+  def questioning(statement: String): Boolean = statement.takeRight(1) == "?"
+
+  def hasLetters(statement: String): Boolean = statement.exists((('a' to 'z') ++ ('A' to 'Z')).toSet.contains(_))
 }
